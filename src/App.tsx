28b9e8803599d3291c81978ff9c1d86c7f213666
brycom <mathiasbrynolf20@gmail.com>
2024-09-17 14:18:32 +0200

    besudlad

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,28 @@
-import "./globals"
-import './App.css'
-import Header from "./header/Header"
-import PlayerInput from "./playerInput/PlayerInput"
-import PlayingField from "./playingField/PlayingField"
-import Scoreboard from "./scoreboard/Scoreboard"
-import PlayerDisplay from "./playerDisplay/PlayerDisplay"
-import { useEffect, useState } from "react"
-import SockJS from "sockjs-client"
-import { Client } from "@stomp/stompjs"
+import "./globals";
+import "./App.css";
+import Header from "./header/Header";
+import PlayerInput from "./playerInput/PlayerInput";
+import PlayingField from "./playingField/PlayingField";
+import Scoreboard from "./scoreboard/Scoreboard";
+import PlayerDisplay from "./playerDisplay/PlayerDisplay";
+import { useEffect, useState } from "react";
+import SockJS from "sockjs-client";
+import { Client } from "@stomp/stompjs";
 
 function App() {
-
-  const [stompClient, setStompClient] = useState<Client | null>(null)
+  const [stompClient, setStompClient] = useState<Client | null>(null);
 
   useEffect(() => {
     sessionStorage.clear();
     if (stompClient) {
       stompClient.publish({
-      destination: "/app/game-end",
-    });
+        destination: "/app/game-end",
+      });
     }
-    const socketUrl = process.env.NODE_ENV === 'development' 
-      ? "http://localhost:8080/connect" 
-      : "https://seal-app-a9r4z.ondigitalocean.app/connect";
+    const socketUrl =
+      process.env.NODE_ENV === "development"
+        ? "http://localhost:8080/connect"
+        : "https://seal-app-a9r4z.ondigitalocean.app/connect";
 
     const socket = new SockJS(socketUrl);
     const client = new Client({
@@ -33,55 +33,81 @@ function App() {
         console.log("Connection established");
       },
       onDisconnect: () => {
-        console.log("disconnected from websocket")
-      }
+        console.log("disconnected from websocket");
+      },
     });
 
     client.activate();
-
-  }, [])
+  }, []);
 
   const resetGame = () => {
     sessionStorage.clear();
     if (stompClient) {
-        stompClient.publish({
+      stompClient.publish({
         destination: "/app/game-end",
-    });
+      });
     }
     location.reload();
-  }
+  };
 
   return (
     <>
-      <button className="resetBtn" onClick={() => resetGame()}>Reset</button>
-      <Header /> 
-      <PlayerInput stompClient={stompClient} /> 
+      <button className="resetBtn" onClick={() => resetGame()}>
+        End Game
+      </button>
+      <Header />
+      <PlayerInput stompClient={stompClient} />
       <PlayerDisplay stompClient={stompClient} />
       <div className="fieldAndScoreboardDiv">
-        <PlayingField stompClient={stompClient} /> 
-        <Scoreboard stompClient={stompClient} />   
-      </div> 
+        <PlayingField stompClient={stompClient} />
+        <Scoreboard stompClient={stompClient} />
+      </div>
       <div>
         <ul className="credList">
           <li>
-            <a href="https://www.flaticon.com/free-icons/bullets" title="bullets icons">Bullets icons created by Freepik - Flaticon</a>
+            <a
+              href="https://www.flaticon.com/free-icons/bullets"
+              title="bullets icons"
+            >
+              Bullets icons created by Freepik - Flaticon
+            </a>
           </li>
           <li>
-            <a href="https://www.flaticon.com/free-icons/hunter" title="hunter icons">Hunter icons created by Freepik - Flaticon</a>
+            <a
+              href="https://www.flaticon.com/free-icons/hunter"
+              title="hunter icons"
+            >
+              Hunter icons created by Freepik - Flaticon
+            </a>
           </li>
           <li>
-            <a href="https://www.flaticon.com/free-icons/spooky" title="spooky icons">Spooky icons created by Freepik - Flaticon</a>
+            <a
+              href="https://www.flaticon.com/free-icons/spooky"
+              title="spooky icons"
+            >
+              Spooky icons created by Freepik - Flaticon
+            </a>
           </li>
           <li>
-            <a href="https://www.flaticon.com/free-icons/viking" title="viking icons">Viking icons created by Freepik - Flaticon</a>
+            <a
+              href="https://www.flaticon.com/free-icons/viking"
+              title="viking icons"
+            >
+              Viking icons created by Freepik - Flaticon
+            </a>
           </li>
           <li>
-            <a href="https://www.flaticon.com/free-icons/witch" title="witch icons">Witch icons created by Funkrataii - Flaticon</a>
+            <a
+              href="https://www.flaticon.com/free-icons/witch"
+              title="witch icons"
+            >
+              Witch icons created by Funkrataii - Flaticon
+            </a>
           </li>
         </ul>
       </div>
-    </> 
-  )
+    </>
+  );
 }
 
-export default App
+export default App;
